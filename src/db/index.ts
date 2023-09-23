f26cde5734148ab42fe8ffc817b40e7e4da59a3b
Prateek Singh <prateek.singh@ultragenicglobal.com>
2023-09-23 22:11:08 +0530

    update package from start

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,30 @@
-import { Pool } from "pg";
-const pool = new Pool();
+import { createConnection } from "typeorm";
+import { DB_CONFIG } from "../../config/config";
+import Logger from "../utility/logger";
 
-export const query = (text: any, params: any = []) => pool.query(text, params);
+class Database {
+  public connect = () => {
+    // Initialize a connection pool against the database.
+    createConnection({
+      type: "postgres",
+      host: DB_CONFIG.host,
+      port: parseInt(DB_CONFIG.port, 10),
+      database: DB_CONFIG.database,
+      username: DB_CONFIG.username,
+      password: DB_CONFIG.password,
+      entities: [
+      ],
+      subscribers: [
+      ],
+      logging: DB_CONFIG.logging === "true",
+      synchronize: DB_CONFIG.sync === "true",
+    })
+      .then(async (connection: any) => {
+        //Check if data is empty create super admin
+        
+        Logger.http(`${DB_CONFIG.database} Database Connected!`);
+        console.log(`DB URL: ${DB_CONFIG.host}`);
+      })
+      .catch((error) => console.log(error));
+  };
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,16 +1,9 @@
 import { Router } from "express";
-import UserController from "../controllers/user/user.controller";
 
 const router = Router();
-const userController = new UserController();
 
 router.get("/status", (req, res) => {
   res.send("Server running successfully");
 });
-
-router.post("/signup", userController.signup);
-router.post("/login", userController.login);
-
-router.get("/view/:id", userController.view);
 
 export default router;
