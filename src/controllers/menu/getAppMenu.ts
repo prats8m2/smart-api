dc1c32326abdba3506a904533174a6661e27747e
prateek.singh <prateek.singh@ultragenicglobal.com>
2024-07-05 00:31:22 +0530

    optimize get menu app code

--- src/controllers/menu/getAppMenu.ts
+++ src/controllers/menu/getAppMenu.ts
@@ -9,6 +9,8 @@ const getAppMenu = async (req: Request, res: Response) => {
 	//fetch data from body
 	const { siteId } = res.locals;
 	Logger.info(`Get App Menu request`);
+	// Create a Set to track checked categories
+	const checkedCategories = new Set();
 
 	//get a menu
 	const menus = await Menu.find({
@@ -17,12 +19,12 @@ const getAppMenu = async (req: Request, res: Response) => {
 	});
 
 	// Extracting product and category IDs from the menuItems
-	const currentMenu = menus.filter((menu) =>
+	const currentMenu = menus.find((menu) =>
 		checkCurrentSchedule(menu.schedule)
 	);
 
 	//select only first menu qualifying criteria
-	const menu = currentMenu[0];
+	const menu = currentMenu;
 
 	// fetch menuItems of menu
 	const menuItems = await Menu.findOne(menu.id, {
@@ -35,21 +37,40 @@ const getAppMenu = async (req: Request, res: Response) => {
 	});
 
 	// Filter menuItems based on category schedule
-	const filteredMenuItems = menuItems.menuItems.filter((item) =>
-		checkCurrentSchedule(item.category.schedule)
-	);
+	const filteredMenuItems = menuItems.menuItems.filter((item) => {
+		const categoryId = item.category.id;
+
+		// If the category has not been checked, check its schedule and add it to the Set
+		if (!checkedCategories.has(categoryId)) {
+			const isCategoryCurrent = checkCurrentSchedule(item.category.schedule);
+
+			// If the category is current, add it to the Set and return true
+			if (isCategoryCurrent) {
+				checkedCategories.add(categoryId);
+				return true;
+			}
+
+			// If the category is not current, add it to the Set and return false
+			checkedCategories.add(categoryId);
+			return false;
+		}
+
+		// If the category has already been checked, assume it was not current
+		return false;
+	});
 
 	// Group products by category
-	const categorizedProducts = filteredMenuItems.reduce((acc: any, item: any) => {
-		const categoryName = item.category.name;
-		if (!acc[categoryName]) {
-			acc[categoryName] = [];
-		}
-		acc[categoryName].push(item.product);
-		return acc;
-	}, {});
-
-
+	const categorizedProducts = filteredMenuItems.reduce(
+		(acc: any, item: any) => {
+			const categoryName = item.category.name;
+			if (!acc[categoryName]) {
+				acc[categoryName] = [];
+			}
+			acc[categoryName].push(item.product);
+			return acc;
+		},
+		{}
+	);
 
 	sendResponse(res, true, CODE.SUCCESS, `Menu App Data`, categorizedProducts);
 };
