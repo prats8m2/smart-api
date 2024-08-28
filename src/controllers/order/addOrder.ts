74302d47fba149598aadf3a07510247d21c7f3e7
prateek.singh <prateek.singh@ultragenicglobal.com>
2024-08-28 13:25:49 +0530

    add error handling in add order

--- src/controllers/order/addOrder.ts
+++ src/controllers/order/addOrder.ts
@@ -9,41 +9,51 @@ import { getTotalPrice } from '../../utility/order/getTotalPrice';
 import sendResponse from '../../utility/response';
 
 const addOrder = async (req: Request, res: Response) => {
-	//fetch data from body
-	const { type, table, site, room, products, categoryType } = req.body;
-	Logger.info(`Add order request`);
-	const io = serverInstance.getIo(); // Get the io instance from the server
+	try {
+		//fetch data from body
+		const { type, table, site, room, products, categoryType } = req.body;
+		Logger.info(`Add order request`);
+		const io = serverInstance.getIo(); // Get the io instance from the server
 
-	//fetch product prices
-	const totalPrice = await getTotalPrice(products);
+		//fetch product prices
+		const totalPrice = await getTotalPrice(products);
 
-	//create payment
-	let payment: Payment = new Payment();
-	payment.type = PAYMENT_TYPE.OFFLINE;
-	payment.site = site;
-	payment.total = totalPrice;
-	const paymentResult = await payment.save();
+		//create payment
+		let payment: Payment = new Payment();
+		payment.type = PAYMENT_TYPE.OFFLINE;
+		payment.site = site;
+		payment.total = totalPrice;
+		const paymentResult = await payment.save();
 
-	//create an order
-	let order: Order = new Order();
-	order.type = type;
-	order.site = site;
-	order.room = room;
-	order.table = table;
-	order.categoryType = categoryType;
-	order.payment = paymentResult;
+		//create an order
+		let order: Order = new Order();
+		order.type = type;
+		order.site = site;
+		order.room = room;
+		order.table = table;
+		order.categoryType = categoryType;
+		order.payment = paymentResult;
 
-	//create order_product
-	const orderResult = await order.save();
-	products.map(async (product: any) => {
-		const orderProduct = new Order_Product();
-		orderProduct.order_id = orderResult.id;
-		orderProduct.product_id = product.id;
-		orderProduct.quantity = product.quantity;
-		await orderProduct.save();
-	});
-	io.emit('orderCreated', order);
-	sendResponse(res, true, CODE.SUCCESS, `Order added Successful`, orderResult);
+		//create order_product
+		const orderResult = await order.save();
+		products.map(async (product: any) => {
+			const orderProduct = new Order_Product();
+			orderProduct.order_id = orderResult.id;
+			orderProduct.product_id = product.id;
+			orderProduct.quantity = product.quantity;
+			await orderProduct.save();
+		});
+		io.emit('orderCreated', order);
+		sendResponse(
+			res,
+			true,
+			CODE.SUCCESS,
+			`Order added Successful`,
+			orderResult
+		);
+	} catch (err) {
+		sendResponse(res, false, CODE.BAD_REQUEST, `Error adding order`);
+	}
 };
 
 export default addOrder;
